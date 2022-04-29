6ef0b06de2ae1fdc4ea2486ebdffa615a5f83dca
Amy Baker <amyraebaker@gmail.com>
2022-04-28 18:07:30 -0700

    Update Character class and complete problem

--- js/homework.js
+++ js/homework.js
@@ -1,3 +1,4 @@
+//JS way chapter 9 Dogs 4/28/22
 class Dog {
     constructor(name, species, size) {
         this.name = name
@@ -16,3 +17,68 @@ console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
 const snowy = new Dog("Snowy", "terrier", 22);
 console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
 console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+
+
+
+//JS way chapter 9 Character inventory 4/28/22
+class Character {
+    constructor(name, health, strength) {
+      this.name = name;
+      this.health = health;
+      this.strength = strength;
+      this.gold = 10
+      this.key = 1
+      this.xp = 0; // XP is always zero for new characters
+    }
+    // Attack a target
+    attack(target) {
+      if (this.health > 0) {
+        const damage = this.strength;
+        console.log(
+          `${this.name} attacks ${target.name} and causes ${damage} damage points`
+        );
+        target.health -= damage;
+        if (target.health > 0) {
+          console.log(`${target.name} has ${target.health} health points left`);
+        } else {
+          target.health = 0;
+          const bonusXP = 10;
+          console.log(
+            `${this
+              .name} eliminated ${target.name} and wins ${bonusXP} experience points, ${target.gold} gold, and ${target.key} key(s)`
+          );
+          this.xp += bonusXP;
+          this.gold += target.gold
+          target.gold = 0
+          this.key += target.key
+          target.key = 0
+        }
+      } else {
+        console.log(`${this.name} can't attack (they've been eliminated)`);
+      }
+    }
+    // Return the character description
+    describe() {
+      return `${this.name} has ${this.health} health points, ${this
+        .strength} as strength, ${this.xp} XP points, ${this.gold} gold, and ${this.key} key(s)`;
+    }
+}
+
+  const aurora = new Character("Aurora", 150, 25);
+  const glacius = new Character("Glacius", 130, 30);
+  
+  console.log("Welcome to the adventure! Here are our heroes:");
+  console.log(aurora.describe());
+  console.log(glacius.describe());
+  
+  const monster = new Character("Spike", 40, 20);
+  console.log("A wild monster has appeared: it's named " + monster.name);
+  
+  monster.attack(aurora);
+  monster.attack(glacius);
+  aurora.attack(monster);
+  glacius.attack(monster);
+  
+  console.log(aurora.describe());
+  console.log(glacius.describe());
+
